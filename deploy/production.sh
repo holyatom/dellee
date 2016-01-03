7b752295ce1e2f3ef7d5345dd47834dd756cdd61
Ablay <atomio.ak@gmail.com>
2016-01-03 08:42:27 +0600

    Fixed deploy scripts

--- deploy/production.sh
+++ deploy/production.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-NODE_ENV=production pm2 start --name "server" ../index.js
-NODE_ENV=production pm2 start --name "worker" ../worker.js
+NODE_ENV=production pm2 start --name "server" ./index.js
+NODE_ENV=production pm2 start --name "worker" ./worker.js
--- deploy/staging.sh
+++ deploy/staging.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-NODE_ENV=staging pm2 start --name "server:staging" ../index.js
-NODE_ENV=staging pm2 start --name "worker:staging" ../worker.js
+NODE_ENV=staging pm2 start --name "server:staging" ./index.js
+NODE_ENV=staging pm2 start --name "worker:staging" ./worker.js
